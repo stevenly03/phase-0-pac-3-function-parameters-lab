8ea27028467734337355a06e93f1e7d2ecba5296
Steven Ly <steven.ly10213@gmail.com>
2021-03-06 15:33:53 -0700

    completed assignment

---
+++ index.js
@@ -0,0 +1,15 @@
+function introduction(name){
+      return `Hi, my name is ${name}.`
+  }
+  introduction('Josh')
+  
+  function introductionWithLanguage(name, language){
+      return `Hi, my name is ${name} and I am learning to program in ${language}.`
+  }
+introductionWithLanguage ('Josh','Ember.js')
+
+function introductionWithLanguageOptional(name, language = ("JavaScript")){
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`
+}
+introductionWithLanguageOptional('Gracie', `${language}`)
+introductionWithLanguageOptional('Gracie', 'Ember.js')
